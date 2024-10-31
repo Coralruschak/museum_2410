c0ae68e4e0752f96ead7c3514f6c5657d533179a
Coral <coralruschak@gmail.com>
2024-10-31 00:05:28 -0400

    watched video for hash alone, exhibits now pass rspec

--- lib/exhibit.rb
+++ lib/exhibit.rb
@@ -1,9 +1,9 @@
 class Exhibit
   attr_reader :name, :cost
 
-  def initialize{name =>; cost=>}
-    @name = name  
-    @cost = cost
+  def initialize(exhibit_info)
+    @name = exhibit_info[:name]  
+    @cost = exhibit_info[:cost]
   end
     
   
